0cfaf9df38a9de9988b9d678012508fe5af1003e
Gena Caesar Athallah <genac75@gmail.com>
2020-02-28 19:10:14 +0700

    Input nama tim dan foto tim, Input nama pencetak gol dan menitnya, Menampilkan foto tim dan nama pencetak gol

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,12 +3,23 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView hasilLaga;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        hasilLaga = findViewById(R.id.textView3);
+
+        Bundle bundle = getIntent().getExtras();
+        String value = bundle.getString("results");
+
+        if(bundle != null){
+            hasilLaga.setText(value);
+        }
     }
 }
