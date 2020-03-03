9aeb5c831ec0be291323c42e93013da914696ad2
Gena Caesar Athallah <genac75@gmail.com>
2020-03-03 18:39:35 +0700

    Menampilkan Hasil Pertandingan pada ResultActivity

--- app/src/main/java/id/putraprima/skorbola/ResultActivity.java
+++ app/src/main/java/id/putraprima/skorbola/ResultActivity.java
@@ -3,23 +3,38 @@ package id.putraprima.skorbola;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import id.putraprima.skorbola.model.Match;
 
 public class ResultActivity extends AppCompatActivity {
 
-    private TextView hasilLaga;
+    private static final String DATA_KEY = "data";
+    TextView tvWinner, tvList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        hasilLaga = findViewById(R.id.textView3);
+        tvWinner = findViewById(R.id.tv_winner);
+        tvList = findViewById(R.id.tv_list);
 
-        Bundle bundle = getIntent().getExtras();
-        String value = bundle.getString("results");
-
-        if(bundle != null){
-            hasilLaga.setText(value);
+        Bundle extras = getIntent().getExtras();
+        Match match = getIntent().getParcelableExtra(DATA_KEY);
+        if (extras != null) {
+            if(match.resultScore().equals(match.getHomeTeam())) {
+                tvWinner.setText("Pemenang Pertandingan : "+ match.resultScore());
+                tvList.setText(match.homeScorer());
+            }else if(match.resultScore().equals(match.getAwayTeam())) {
+                tvWinner.setText("Pemenang Pertandingan : "+ match.resultScore());
+                tvList.setText(match.awayScorer());
+            }else{
+                tvWinner.setText(match.resultScore());
+                tvList.setText("Pertandingan Berakhir Seri");
+            }
         }
     }
 }
